b92563387745135050cf5bdba910244df70d7d86
Alexia Balionis <balionisalexia@gmail.com>
2018-04-16 18:04:32 -0300

    changes to the model

--- models/politico.js
+++ models/politico.js
@@ -6,14 +6,20 @@ var PoliticoSchema = new mongoose.Schema({
     pessoa: { type: mongoose.Schema.Types.ObjectId, ref: 'pessoas' },
     // partido: { type: mongoose.Schema.Types.ObjectId, ref: 'partido' },
     partido: { type: String, required: true },
+    cnpj: { type: Number },
+    datanascimento: {type: Date},
+    emaileleitoral: { type: String, required: true },
+    estado: { type: String, required: true },
+    nome_eleitoral: { type: String, required: true },
     despesa_campanha: { type: Number },
     escolaridade: { type: String, required: true },
-    qtd_votos: { type: Number, required: true },
+    qtd_votos: { type: Number },
     data_eleito: { type: Date },
-    numero_candidato: { type: Number, required: true },
+    numero_candidato: { type: Number },
     qtd_intencao_votos: { type: Number },
     biografia: { type: String },
-    cargo: { type: String }
+    cargo_politico: { type: String },
+    perfil_aprovado: { type: Boolean, required: true }
 });
 
 var Politico = mongoose.model('politicos', PoliticoSchema);
