1344746b2deca0058837cf619e2d73c1a7c20e23
abalioni <>
2018-05-06 01:24:26 -0300

    Fill pesquisa.politicos.politico when getting a pesquisa

--- routes/voto360-pesquisa-get/processor/processor.js
+++ routes/voto360-pesquisa-get/processor/processor.js
@@ -1,15 +1,20 @@
 module.exports.executa = (req, context, done) => {
   if(req.params.id_pesquisa) {
     const condition = {_id: req.params.id_pesquisa };
-    context.pesquisaModel.findOne(condition, (err, pesquisa) => {
-      if (err) {
-        done(err);
-      } else {
-        done(null, pesquisa);
-      }
-    });
+    context.pesquisaModel
+      .findOne(condition)
+      .populate('politicos.politico')
+      .exec((err, pesquisa) => {
+        if (err) {
+          done(err);
+        } else {
+          done(null, pesquisa);
+        }
+      });
   } else {
-    context.pesquisaModel.find({}, (err, pesquisa) => {
+    context.pesquisaModel.find({})
+    .populate('politicos.politico')
+    .exec((err, pesquisa) => {
       if (err) {
         done(err);
       } else {
